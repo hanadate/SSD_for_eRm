9cf04b5af05b8f12b0660434dd8b8bbf602bf86e
hanadate <s2421310@u.tsukuba.ac.jp>
2025-01-04 01:01:40 +0900

    Update ssd_for_extended_rasch.R

--- ssd_for_extended_rasch.R
+++ ssd_for_extended_rasch.R
@@ -11,8 +11,8 @@ library(eRm)
 library(mixedpower)
 library(doMPI)
 #===== register cores
-cores <- 56-1
-cl <- startMPIcluster(count=cores)
+cores <- 56
+cl <- startMPIcluster(count=cores-1)
 registerDoMPI(cl)
 #===== LLTM
 raschdat1_long <- raschdat1 %>% 
@@ -36,11 +36,11 @@ t<-proc.time()
 power.rasch <- mixedpower(model=glmer.rasch, data=raschdat1_long,
                           fixed_effects=c("item"),
                           simvar="person", steps=c(10,30,50),
-                          critical_value=2, n_sim=10,
+                          critical_value=2, n_sim=1000,
                           SESOI=FALSE, databased=TRUE,
                           maxCores=cores)
 proc.time()-t
 #=== laptop(AMD Ryzen 7 7730U with Radeon Graphics 2.00 GH 16 threads): 
-#= steps 6 x n_sim 10: 5 mins
+#= steps 6 x n_sim 10: 5 mins, 1000: x mins.
 saveRDS(power.rasch, "power_rasch.rds")
 closeCluster(cl)
