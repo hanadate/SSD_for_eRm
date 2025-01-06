f68a76e0285ee4cbd0f4c685ff9ea4d766b8f609
hanadate <hanadate.shuhei.tkb_gb@u.tsukuba.ac.jp>
2025-01-06 20:32:15 +0900

    print chunksize

--- ssd_for_extended_rasch.R
+++ ssd_for_extended_rasch.R
@@ -37,14 +37,15 @@ t<-proc.time()
 print(paste0("hosts_num: ",hosts_num(hosts="hosts")))
 print(paste0("cores_num: ",cores_num(hosts="hosts")))
 n_sim<-100
-print(paste0("chunkSize: ",floor(n_sim/(2*hosts_num(hosts="hosts")))))
+chunkSize <- floor(n_sim/(1*hosts_num(hosts="hosts")))
+print(paste0("chunkSize: ",chunkSize))
 power.rasch <- mixedpower_mpi(model=glmer.rasch, data=raschdat1_long,
                           fixed_effects=c("item"),
                           simvar="person", steps=c(10,30,50),
                           critical_value=2, n_sim=n_sim,
                           SESOI=FALSE, databased=TRUE,
                           maxCores=hosts_num(hosts="hosts"),
-                          chunkSize=floor(n_sim/(1*hosts_num(hosts="hosts"))))
+                          chunkSize=chunkSize)
 proc.time()-t
 #=== laptop(AMD Ryzen 7 7730U with Radeon Graphics 2.00 GH 16 threads): 
 #= steps=3 & n_sim=10: 3 mins, steps=3 & n_sim=100: 15 mins
