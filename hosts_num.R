f9946473f57609fa98c38191156a9a559640465e
hanadate <s2421310@u.tsukuba.ac.jp>
2025-01-06 10:38:31 +0900

    Update hosts_num.R

--- hosts_num.R
+++ hosts_num.R
@@ -1,6 +1,12 @@
-hosts_num <- 
+cores_num <- 
   function(hosts="hosts"){
     hosts_table <- read.table(hosts, header=FALSE)
     nCores <- sum(as.integer(gsub("cpu=","",hosts_table$V2)))
     return(nCores)
+  }
+hosts_num <- 
+  function(hosts="hosts"){
+    hosts_table <- read.table(hosts, header=FALSE)
+    nHosts <- nrow(hosts_table)
+    return(nHosts)
   }
