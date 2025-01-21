28547659d8f7e80793e7e48eff4f28bee2dc0a37
hanadate <hanadate.shuhei.tkb_gb@u.tsukuba.ac.jp>
2025-01-21 13:12:32 +0900

    run mixedpower for RM

--- ssd_for_extended_rasch.R
+++ ssd_for_extended_rasch.R
@@ -3,7 +3,7 @@
 #   install.packages("devtools", dependencies = TRUE)}
 # devtools::install_github("DejanDraschkow/mixedpower") 
 # install.packages("lme4")
-
+Sys.setenv(LANGUAGE="en")
 #===== Libraries
 library(tidyverse)
 library(lme4)
@@ -11,81 +11,57 @@ library(eRm)
 library(mixedpower)
 library(doParallel)
 library(mirt)
-# #===== LLTM
-# raschdat1_long <- raschdat1 %>% 
-#   mutate(person=rownames(.)) %>% 
-#   pivot_longer(cols=starts_with("I"), names_to="item", values_to="resp") %>% 
-#   mutate(person=as.integer(person))
-# items <- unique(raschdat1_long$item)
-# number_of_items <- length(items)
-# # Fit the model using glmer
-# t<-proc.time()
-# glmer.rasch <- glmer(resp ~ item + (1 | person), 
-#                      data = raschdat1_long, 
-#                      family = binomial,
-#                      control=glmerControl(optCtrl=list(maxfun=100000))
-# )
-# proc.time()-t #41sec
-# summary(glmer.rasch)
-# 
-# # power analysis
-# t<-proc.time()
-# power.rasch <- mixedpower(model=glmer.rasch, data=raschdat1_long,
-#                           fixed_effects=c("item"),
-#                           simvar="person", steps=c(10,30,50),
-#                           critical_value=2, n_sim=10,
-#                           SESOI=FALSE, databased=TRUE)
-# proc.time()-t
-# # nsim 10: 197sec(thinkpad), 62sec(a311), sec(ishioka)
-# 
-# #=== laptop(AMD Ryzen 7 7730U with Radeon Graphics 2.00 GH 16 threads): 
-# #= n_sim=10: 3 mins, n_sim=100: 15 mins, n_sim=1000: 150 mins
-# #=== 56 threads (8 workers)
-# #= n_sim=100 & chunkSize=*1: 16 min 
-# #= n_sim=100 & chunkSize=*2: 16 min 
-# #= n_sim=1000 & chunkSize=*2: 155 min
+library(stargazer)
 
+#===== RM, RSM
+#====
 model<-c("RM","RSM")
-a<-seq(-1,1,.5) # Discrimination parameters
+a<-seq(0,1,.5) # Discrimination parameters
 d<-seq(-1,1,.5) # Difficulty parameters
 ad<-expand.grid(a,d) %>% 
   `colnames<-`(c("a","d")) %>% 
   mutate(a_d=paste0(a,"_",d))
 N<-seq(50,200,50)
 
+# create data
 set.seed(1)
 x1 <- simdata(a=ad$a,d=ad$d,N=max(N),itemtype="dich") %>% 
   as.data.frame() %>% 
   `colnames<-`(ad$a_d) %>% 
-  mutate(`00_ref`=0) %>% 
-  select(`00_ref`, everything())
+  mutate(`!!`=0) %>% 
+  select(`!!`, everything())
 set.seed(2)
 x2 <- simdata(a=ad$a,d=ad$d,N=max(N),itemtype="dich") %>% 
   as.data.frame() %>%
   `colnames<-`(ad$a_d) %>% 
-  mutate(`00_ref`=0)%>% 
-  select(`00_ref`, everything())
-x3 <- x1+x2
+  mutate(`!!`=0)%>% 
+  select(`!!`, everything())
+x3 <- x1+x2 #%>% 
+  # dendrify
+  
 
 raschdat_long <- x1 %>% 
   mutate(person=rownames(.)) %>%
-  pivot_longer(cols=contains("_"), names_to="item", values_to="resp") %>% 
-  mutate(person=as.integer(person))
-
+  pivot_longer(cols=-person, names_to="item", values_to="resp") %>% 
+  mutate(person=as.integer(person),
+         item=factor(item))
+raschdat_long$item <- factor(raschdat_long$item, levels=colnames(x1))
 t <- proc.time()
-glmer.rasch <- glmer(resp ~ -1 + item + (1 | person), 
+(glmer.rasch <- glmer(resp ~ 0 + item + (1 | person), 
                      data = raschdat_long, 
-                     family = binomial)
-proc.time()-t #17sec
+                     family = binomial))
+proc.time()-t #15sec
 saveRDS(glmer.rasch, "glmer_rasch.rds")
 glmer.rasch <- readRDS("glmer_rasch.rds")
+
 t<-proc.time()
 power.rasch <- mixedpower(model=glmer.rasch, data=raschdat_long,
                           fixed_effects=c("item"),
                           simvar="person", steps=N,
                           critical_value=2, n_sim=1000,
                           SESOI=FALSE, databased=TRUE)
-proc.time()-t #80sec
+proc.time()-t 
+# Core(TM) i9-12900   2.40 GHz: 3.2hour9
 saveRDS(power.rasch, "power_rasch.rds")
 power.rasch <- readRDS("power_rasch.rds")
 power.rasch
@@ -95,3 +71,20 @@ multiplotPower(power.rasch)
   mutate(effect=str_remove(effect, "item")) %>% 
   separate_wider_delim(effect,delim="_",names=c("a","d")))
 
+# sample size 50
+(pwr.N50.rasch <- power.rasch.ad %>% 
+  select(`50`,a,d) %>% 
+  filter(a>=0) %>% 
+  pivot_wider(names_from=a, values_from=`50`) %>% 
+  as.matrix()
+)
+stargazer(pwr.N50.rasch)
+(pwr.N200.rasch <- power.rasch.ad %>% 
+    select(`200`,a,d) %>% 
+    filter(a>=0) %>% 
+    pivot_wider(names_from=a, values_from=`200`) %>% 
+    as.matrix()
+)
+stargazer(pwr.N200.rasch)
+#=====
+#===== LLTM, LRSM
